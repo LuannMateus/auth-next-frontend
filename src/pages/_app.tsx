0b990909292ba3e101047805e4fd0c9f027321f1
LuannMateus <luanmateuslima34@gmail.com>
2022-03-13 18:25:15 -0300

    Add nextjs-progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,18 @@ import { theme } from '../styles/theme';
 import { GlobalStyles } from '../styles/globalStyles';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider theme={theme}>
+        <NextNProgress
+          color={theme.colors.info}
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={10}
+        />
         <Component {...pageProps} />
         <GlobalStyles />
       </ThemeProvider>
